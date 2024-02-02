5937c6d9478a091062549cd3661c2b9cb28d7bb0
mrcyme <simeonmcl@gmail.com>
2024-02-02 16:42:19 +0100

    Add default language, Strapi URL, and page history variables, and implement function to load content from Strapi API.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let language = "en"; // Default to English
+let strapiUrl = "http://46.226.110.124:1337";
+let pageHistory = ["home"]; // Start with the home page
+
+let interfaceContent;
+async function loadContentFromStrapi() {
+  try {
+    const response = await fetch(
+      `${strapiUrl}/api/totem-interface-items?locale=${language}&populate=*`
+    );
+    if (!response.ok) {
+      console.log(response);
+    }
+    interfaceContent = await response.json();
+  } catch (error) {
+    console.error("Error occurred: ", error);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const navbar = document.getElementById('navbar');
     navbar.addEventListener('click', function(event) {
